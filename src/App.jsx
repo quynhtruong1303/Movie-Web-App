dba4b71d983678121a5974bdfb13d91afb9b0bab
Nha Truong <quynhtruong1303@gmail.com>
2025-10-06 11:41:20 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./public/hero.png" alt="Hero Banner" className="hero" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
+        </header>
+        
+      </div>
+    </main>
+  )
+}
+
+export default App
